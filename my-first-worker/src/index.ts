4192dc35bffdd74cf2cf09ec23cd1482d5c3e5ed
Anish Ummenthala <ummenthala.anish@gmail.com>
2026-04-14 17:31:27 -0700

    build worker ai for llm integration

--- my-first-worker/src/index.ts
+++ my-first-worker/src/index.ts
@@ -1,15 +1,30 @@
 import { Hono } from 'hono';
 
-export interface Env {}
+export interface Env {
+	AI: Ai;
+}
 
-const app = new Hono<{Bindings: Env}>();
+const app = new Hono<{ Bindings: Env }>();
 
-app.get( '/', (c) => {
-		console.log(c.req);
-		return c.json({
-			message: 'Hello World!',
-		});
-	}
-);
+// GET /?query="How is your day today?"
+app.get('/', async (c) => {
+	// const url = new URL(c.req.url);
+	// const content = url.searchParams.get('query');
+
+	const ai = c.env.AI;
+
+	const content = c.req.query('query') || 'How is your day today?';
+
+	const messages = [
+		{ role: 'system', content: 'You are a helpful assistant.' },
+		{ role: 'user', content },
+	];
+
+	const res = await ai.run('@cf/mistral/mistral-7b-instruct-v0.1', {
+		messages,
+	});
+
+	return c.json(res);
+});
 
 export default app;
